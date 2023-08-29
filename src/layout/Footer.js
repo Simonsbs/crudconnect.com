df6f351cd4fc3a15ef31e3a5ae92bc30ef699658
Simon B.Stirling <simonsbs@gmail.com>
2023-08-29 03:29:28 +0300

    .

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,14 +4,16 @@ function Footer() {
   return (
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-        <p className="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+        <p className="col-md-4 mb-0 text-body-secondary">
+          © 2023 CRUDConnect.com
+        </p>
         <Link
           to="/"
           className="col-md-4 d-flex align-items-center
           justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis
           text-decoration-none"
         >
-          LOGO
+          CRUDConnect
         </Link>
 
         <ul className="nav col-md-4 justify-content-end">
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -32,7 +32,7 @@ function Header() {
             to="./"
             className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none"
           >
-            LOGO
+            CRUDConnect
           </Link>
 
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
